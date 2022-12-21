da18579680741d085759b1eddee09d154c1cde92
ERCAN <yilmazercan404@gmail.com>
2022-12-22 00:42:18 +0300

    Proje dosyası ekle.

---
+++ form dersleri bilgi yarışması/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace form_dersleri_bilgi_yarışması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(685, 204);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.UseWaitCursor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(42, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 63);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(437, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 63);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseWaitCursor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(42, 392);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(213, 63);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseWaitCursor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(437, 382);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(213, 63);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseWaitCursor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(1137, 302);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "TRUE";
+            this.label1.UseWaitCursor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(1137, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(213, 63);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "START";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseWaitCursor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(1117, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 31);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "POINT:";
+            this.label2.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(1239, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 31);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "00";
+            this.label3.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(1239, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 31);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "00";
+            this.label4.UseWaitCursor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(1058, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(165, 31);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "QUESTION:";
+            this.label5.UseWaitCursor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(1219, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 31);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "00";
+            this.label6.UseWaitCursor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(1058, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(166, 31);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "DURATİON:";
+            this.label7.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1364, 643);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "------------------------------------------------------------------------------KNO" +
+    "WLEDGE COMPETITION--------------------------------------------------------------" +
+    "------------------";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ form dersleri bilgi yarışması/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.CodeDom;
+
+namespace form_dersleri_bilgi_yarışması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connect = new SqlConnection("Data Source=ERCANMONSTER\\ERCANSERVER;Initial Catalog=sorularım;Integrated Security=True");
+        int COUNTER = 0;
+        int point = 0;
+        int time = 21;
+      /*  private void gorunurluluk()
+        {
+            button5.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+        }*/
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            time = 21;
+            button1.BackColor = Color.Gray;
+            button2.BackColor = Color.Gray;
+            button3.BackColor = Color.Gray;
+            button4.BackColor = Color.Gray;
+            button5.Enabled = false;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Text = "NEXT";
+            COUNTER++;
+            label4.Text = COUNTER.ToString();
+            if (COUNTER == 1)
+            {
+                connect.Open();
+                SqlCommand command = new SqlCommand("Select * from soru1 order by NEWID()", connect);
+                SqlDataReader read = command.ExecuteReader();
+                while (read.Read())
+                {
+                    button1.Text = (read["a"].ToString());
+                    button2.Text = (read["b"].ToString());
+                    button3.Text = (read["c"].ToString());
+                    button4.Text = (read["d"].ToString());
+                    label1.Text = (read["doğru"].ToString());
+                    textBox1.Text = (read["soru"].ToString());
+
+
+
+                }
+                connect.Close();
+            }
+            if (COUNTER == 2)
+            {
+                connect.Open();
+                SqlCommand command = new SqlCommand("Select * from soru2 order by NEWID()", connect);
+                SqlDataReader read = command.ExecuteReader();
+                while (read.Read())
+                {
+                    button1.Text = (read["a"].ToString());
+                    button2.Text = (read["b"].ToString());
+                    button3.Text = (read["c"].ToString());
+                    button4.Text = (read["d"].ToString());
+                    label1.Text = (read["doğru"].ToString());
+                    textBox1.Text = (read["soru"].ToString());
+                }
+                connect.Close();
+            }
+            if (COUNTER == 3)
+            {
+                connect.Open();
+                SqlCommand command = new SqlCommand("Select * from soru3 order by NEWID()", connect);
+                SqlDataReader read = command.ExecuteReader();
+                while (read.Read())
+                {
+                    button1.Text = (read["a"].ToString());
+                    button2.Text = (read["b"].ToString());
+                    button3.Text = (read["c"].ToString());
+                    button4.Text = (read["d"].ToString());
+                    label1.Text = (read["doğru"].ToString());
+                    textBox1.Text = (read["soru"].ToString());
+                }
+                connect.Close();
+            }
+            if (COUNTER == 4)
+            {
+                connect.Open();
+                SqlCommand command = new SqlCommand("Select * from soru4 order by NEWID()", connect);
+                SqlDataReader read = command.ExecuteReader();
+                while (read.Read())
+                {
+                    button1.Text = (read["a"].ToString());
+                    button2.Text = (read["b"].ToString());
+                    button3.Text = (read["c"].ToString());
+                    button4.Text = (read["d"].ToString());
+                    label1.Text = (read["doğru"].ToString());
+                    textBox1.Text = (read["soru"].ToString());
+                }
+                connect.Close();
+            }
+            if (COUNTER == 5)
+            {
+                connect.Open();
+                SqlCommand command = new SqlCommand("Select * from soru5 order by NEWID()",connect);
+                SqlDataReader read =command.ExecuteReader();
+                while (read.Read())
+                {
+                    button1.Text = (read["a"].ToString());
+                    button2.Text = (read["b"].ToString());
+                    button3.Text = (read["c"].ToString());
+                    button4.Text = (read["d"].ToString());
+                    label1.Text = (read["doğru"].ToString());
+                    textBox1.Text = (read["soru"].ToString());
+                }
+                connect.Close();
+            }
+            if (COUNTER == 6)
+            {
+                connect.Open();
+                SqlCommand command = new SqlCommand("Select * from soru6  order by NEWID()",connect);
+                SqlDataReader read = command.ExecuteReader();
+                while (read.Read())
+                {
+                    button1.Text = (read["a"].ToString());
+                    button2.Text = (read["b"].ToString());
+                    button3.Text = (read["c"].ToString());
+                    button4.Text = (read["d"].ToString());
+                    label1.Text = (read["doğru"].ToString());
+                    textBox1.Text = (read["soru"].ToString());
+                    
+                }
+                connect.Close();
+            }
+            if (COUNTER == 7)
+            {
+                connect.Open();
+                button5.Text = "GAME OVER";
+                button5.Enabled = false;
+
+                SqlCommand command = new SqlCommand("Select * from soru7 order by NEWID()",connect);
+                SqlDataReader read = command.ExecuteReader();
+                while (read.Read())
+                {
+                    button1.Text = (read["a"].ToString());
+                    button2.Text = (read["b"].ToString());
+                    button3.Text = (read["c"].ToString());
+                    button4.Text = (read["d"].ToString());
+                    label1.Text = (read["doğru"].ToString());
+                    textBox1.Text = (read["soru"].ToString());
+
+                }
+                connect.Close();
+            }
+            if (COUNTER == 8)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+            }
+        }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                label4.Text = COUNTER.ToString();
+
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == label1.Text)
+            {
+                point = point + 10;
+                label3.Text = point.ToString();
+                button1.BackColor = Color.DarkGreen;
+            }
+            else
+            {
+                button1.BackColor = Color.Red;
+            }
+            button5.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == label1.Text)
+            {
+                point = point + 10;
+                label3.Text = point.ToString();
+                button3.BackColor = Color.DarkGreen;
+            }
+            else
+            {
+                button3.BackColor = Color.Red;
+            }
+            button5.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == label1.Text)
+            {
+                point = point + 10;
+                label3.Text = point.ToString();
+                button2.BackColor = Color.DarkGreen;
+            }
+            else
+            {
+                button2.BackColor = Color.Red;
+            }
+            button5.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == label1.Text)
+            {
+                point = point + 10;
+                label3.Text = point.ToString();
+                button4.BackColor = Color.DarkGreen;
+            }
+            else
+            {
+                button4.BackColor = Color.Red;
+
+            }
+            button5.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time = time - 1;
+            label6.Text = time.ToString();
+            if (time == 0)
+            {
+                timer1.Enabled = false;
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+            }
+        }
+    }
+}
